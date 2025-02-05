49e49fe1cf492f54458372988c87a9eae78c2040
red4626 <red@infrared.finance>
2025-02-05 11:47:35 -0500

    feat: cmd test

---
+++ cmd/test/dolomite/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"os"
+	"strings"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/infrared-dao/protocols"
+	"github.com/rs/zerolog"
+	"github.com/shopspring/decimal"
+)
+
+func main() {
+	// Create a zerolog logger
+	logger := zerolog.New(os.Stdout).With().
+		Timestamp().
+		Logger()
+
+	// Command-line arguments
+	addressArg := flag.String("address", "", "Smart contract address")
+	price0Arg := flag.String("price0", "", "address:price of token 0, colon delimited")
+	rpcURLArg := flag.String("rpcurl", "https://mainnet.infura.io/v3/YOUR_INFURA_PROJECT_ID", "Ethereum RPC URL")
+	flag.Parse()
+
+	// Validate required arguments
+	missingArgs := []string{}
+	if *addressArg == "" {
+		missingArgs = append(missingArgs, "address")
+	}
+	if *price0Arg == "" {
+		missingArgs = append(missingArgs, "price0")
+	}
+	if len(missingArgs) > 0 {
+		logger.Fatal().
+			Strs("missingArgs", missingArgs).
+			Str("usage", "go run main.go -address <contract-address> -abipath <path-to-abi> -price0 <price0> -price1 <price1> -rpcurl <rpc-url>").
+			Msg("Missing required arguments")
+	}
+
+	// Parse prices
+	p0data := strings.Split(*price0Arg, ":")
+	if len(p0data) != 2 {
+		logger.Fatal().Msgf("Invalid price0, '%s'", *price0Arg)
+	}
+	price0, err := decimal.NewFromString(p0data[1])
+	if err != nil {
+		logger.Fatal().Err(err).Str("price0", *price0Arg).Msg("Invalid price0")
+	}
+	pmap := map[string]protocols.Price{
+		strings.ToLower(p0data[0]): protocols.Price{Decimals: 18, Price: price0},
+	}
+
+	ctx := context.Background()
+	// Connect to the Ethereum client
+	client, err := ethclient.Dial(*rpcURLArg)
+	if err != nil {
+		logger.Fatal().Err(err).Str("rpcurl", *rpcURLArg).Msg("Failed to connect to Ethereum client")
+	}
+
+	cp := protocols.DolomiteLPPriceProvider{}
+	configBytes, err := cp.GetConfig(ctx, *addressArg, client)
+	if err != nil {
+		logger.Fatal().Err(err).Str("address", *addressArg).Msg("Failed to get config")
+	}
+
+	// Parse the smart contract address
+	address := common.HexToAddress(*addressArg)
+	// Create a new DolomiteLPPriceProvider
+	provider := protocols.NewDolomiteLPPriceProvider(address, pmap, logger, configBytes)
+
+	// Initialize the provider
+	err = provider.Initialize(ctx, client)
+	if err != nil {
+		logger.Fatal().Err(err).Msg("Failed to initialize KodiakLPPriceProvider")
+	}
+
+	// Fetch LP token price
+	lpPrice, err := provider.LPTokenPrice(ctx)
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to fetch LP token price")
+	} else {
+		logger.Info().
+			Str("LPTokenPrice (USD)", lpPrice).
+			Msg("successfully fetched LP token price")
+	}
+
+	// Fetch TVL
+	tvl, err := provider.TVL(ctx)
+	if err != nil {
+		logger.Error().Err(err).Msg("Failed to fetch TVL")
+	} else {
+		logger.Info().
+			Str("TVL (USD)", tvl).
+			Msg("successfully fetched TVL")
+	}
+
+}
